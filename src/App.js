d7c79865f9de6a204a194184a6477dbba4c516b5
Syed Ateeq <cs172011@dsu.edu.pk>
2021-03-26 16:48:18 +0500

    one more update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. <br> </br> Update by Ateeq
+          Edit <code>src/App.js</code> and save to reload.  
+           Update by Ateeq
         </p>
         <a
           className="App-link"
